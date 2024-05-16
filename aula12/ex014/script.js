a6aa11da8b537ccda1b96c68fe40fb44b4f88d56
Eduardo Augusto <163478183+EduardoAugustoBF@users.noreply.github.com>
2024-05-17 00:25:45 +0100

    projeto de horário

--- aula12/ex014/script.js
+++ aula12/ex014/script.js
@@ -1,2 +1,20 @@
-var msg = window.document.getElementById('msg')
-var img = window.document.getElementById('foto')
+function carregar() {
+  var msg = window.document.getElementById("msg");
+  var img = window.document.getElementById("imagem");
+  var data = new Date()
+  var hora = data.getHours()
+  msg.innerHTML = `Agora são ${hora} horas.`
+  if (hora >= 0 && hora < 12) {
+    //BOM DIA!
+    document.body.style.background = '#DBCB18'
+    img.src = 'manha.jpg'
+  } else if (hora >= 12 && hora < 18) {
+    //BOA TARDE!
+    document.body.style.background = '#b9846f'
+    img.src = 'tarde.jpg'
+  } else {
+    //BOA NOITE!
+    document.body.style.background = '#515154'
+    img.src = 'noite.jpg'
+  }
+} 
