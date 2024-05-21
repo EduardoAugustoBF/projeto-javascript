213e1b241f3ca9229f19f84ae820b10a0c581259
Eduardo Augusto <163478183+EduardoAugustoBF@users.noreply.github.com>
2024-05-21 01:02:53 +0100

    fotos

--- aula12/modelo/script.js
+++ aula12/modelo/script.js
@@ -16,27 +16,35 @@ function verificar() {
       gênero = "Homem";
       if (idade >= 0 && idade < 10) {
         //Criança
-        img.setAttribute("src", "menino.crianca.jpg");
+        img.setAttribute("src", "foto-bebe-m.jpg");
       } else if (idade < 21) {
         //Jovem
+        img.setAttribute("src", "foto-jovem-m.jpg");
       } else if (idade < 50) {
         //Adulto
+        img.setAttribute("src", "foto-adulto-m.jpg");
       } else {
         //Idoso
+        img.setAttribute("src", "foto-idoso-m.jpg");
       }
     } else if (fsex[1].checked) {
       gênero = "Mulher";
       if (idade >= 0 && idade < 10) {
         //Criança
+        img.setAttribute("src", "foto-bebe-f.jpg");
       } else if (idade < 21) {
         //Jovem
+        img.setAttribute("src", "foto-jovem-f.jpg");
       } else if (idade < 50) {
         //Adultas
+        img.setAttribute("src", "foto-adulta-f.jpg");
       } else {
         //Idosa
+        img.setAttribute("src", "foto-idosa-f.jpg");
       }
     }
     res.style.textAlign = "center";
     res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`;
+    res.appendChild(img);
   }
 }
